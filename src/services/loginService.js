9304c5f852a2ff9d2e74749d28f403eea2aa75dd
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-06-13 22:10:59 -0300

    feat: create login services

---
+++ src/services/loginService.js
@@ -0,0 +1,26 @@
+const { User } = require('../database/models');
+const { generateJWTToken } = require('../helpers/JWTToken');
+// const createMessage = require('../messages');
+
+const invalidField = { status: 401, message: 'Invalid fields' };
+
+const authentication = async ({ email, password }) => {
+    if (!email || !password) {
+        throw new Error({ status: 401, message: 'Some required fields are missing' });
+    }
+
+    const getUser = await User.findOne({
+        where: { email, password },
+    });
+
+    if (!getUser) {
+        throw invalidField;
+    }
+
+    const token = generateJWTToken(getUser.dataValues);
+    return { token };
+};
+
+module.exports = {
+    authentication,
+}; 
