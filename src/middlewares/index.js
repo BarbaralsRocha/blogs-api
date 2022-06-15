2d94bb1d61c2db26526b036cea97a0605e9a2505
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-06-15 07:59:00 -0300

    feat: new middleware to validate user post

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,9 @@
 const error = require('./errorHandler');
 const authentication = require('./authentication');
+const userValidation = require('./userValidation');
 
 module.exports = {
     error,
     authentication,
+    userValidation,
 };
---
+++ src/middlewares/userValidation.js
@@ -0,0 +1,30 @@
+const Joi = require('joi');
+
+const productsDTO = Joi.object({
+    displayName: Joi.string().min(8).required().messages({
+        'any.required': '400|{{#label}} is required',
+        'string.min': '400|{{#label}} length must be at least 8 characters long',
+    }),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required().messages({
+        'any.required': '400|{{#label}} is required',
+        'string.min': '400|{{#label}} length must be at least 6 characters long',
+    }),
+    image: Joi.string().min(8).required(),
+}).required().messages({
+    'any.required': '400|{{#label}} is required',
+    'string.base': '400| must be a string',
+    'number.base': '422|{{#label}} must be a number',
+    'email.base': '422|{{#label}} must be a valid email',
+});
+
+const userValidation = (req, res, next) => {
+    const { error } = productsDTO.validate(req.body);
+    if (error) {
+        const [code, message] = error.message.split('|');
+        return res.status(code).json({ message });
+    }
+    next();
+};
+
+module.exports = userValidation;
