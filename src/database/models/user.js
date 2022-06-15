be2484ca19cc03e245eb8b03234819831328aa43
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-06-15 07:58:12 -0300

    feat: add associations and id

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,10 +1,19 @@
 const UsersSchema = (sequelize, DataTypes) => {
   const UsersTable = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING
   });
+
+  UsersTable.associate = (models) => {
+    UsersTable.hasMany(models.BlogPost, { foreignKey: "userId", as: "blogPosts" });
+  }
+  
   return UsersTable;
 }
 
