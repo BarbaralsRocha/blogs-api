8cbf6f4bf23a375ca1a8c9ecbba6ba361ba747ad
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-06-15 07:57:16 -0300

    fix: delete archives created with erros

--- src/database/models/categories.js
+++
@@ -1,8 +0,0 @@
-const categoriesSchema = (sequelize, DataTypes) => {
-  const categoriesTable = sequelize.define("Categories", {
-    name: DataTypes.STRING
-  });
-  return categoriesTable;
-}
-
-module.exports = categoriesSchema;
