848ccbf81b9d6beb106271380f1d0f4341f24cd0
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-06-15 10:03:47 -0300

    feat: middleware index category

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 const error = require('./errorHandler');
 const authentication = require('./authentication');
 const userValidation = require('./userValidation');
+const categoryValidation = require('./categoryValidation');
 
 module.exports = {
     error,
     authentication,
     userValidation,
+    categoryValidation,
 };
