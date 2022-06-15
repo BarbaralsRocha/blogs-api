fe15d7e8ee8273e94c8a2c2cf72741fccadd52fa
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-06-15 07:57:38 -0300

    feat: create new model category

---
+++ src/database/models/category.js
@@ -0,0 +1,12 @@
+const categoriesSchema = (sequelize, DataTypes) => {
+  const categoriesTable = sequelize.define("Category", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING
+  });
+  return categoriesTable;
+}
+
+module.exports = categoriesSchema;
