e51bd09869e2dc5aa864726fb4fcce25401618e1
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-06-15 10:05:16 -0300

    feat: add route category

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 const routeLogin = require('./login');
 const routeUser = require('./user');
+const routeCategories = require('./categories');
 
 router.use('/login', routeLogin);
 router.use('/user', routeUser);
+router.use('/categories', routeCategories);
 
 router.use(errorHandler.error);
 
