7179c1c213390001f8e39480a1b77900093ee0d3
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-06-15 10:00:42 -0300

    refact: delete unecessary code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,10 +2,27 @@ const userService = require('../services/userService');
 
 const newUser = async (req, res) => {
     const token = req.headers.authorization;
-    const user = await userService.createUser(req.body);
-    if (user) {
+    try {
+        await userService.createUser(req.body);
         return res.status(200).json({ token });
+    } catch (e) {
+        return res.status(409).json({ message: 'User already registered' });
     }
 };
 
-module.exports = newUser;
+const getUsers = async (req, res) => {
+    const user = await userService.getUsers(req.body);
+    return res.status(200).json(user);
+};
+
+const getUsersById = async (req, res) => {
+    const user = await userService.getUsersById(req.params.id);
+    if (!user) return res.status(404).json({ message: 'User does not exist' });
+    return res.status(200).json(user);
+};
+
+module.exports = {
+    newUser,
+    getUsers,
+    getUsersById,
+};
