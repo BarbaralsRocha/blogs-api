8462493b2057c8783bfaaee58e4e430331879089
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-06-20 15:59:03 -0300

    feat: create newPost and getPostById

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,15 +1,23 @@
 const postService = require('../services/postService');
 
-// const newCategory = async (req, res) => {
-//     const { name } = await postService.createCategory(req.body);
-//     return res.status(201).json({ name });
-// };
+const newPost = async (req, res) => {
+    const post = await postService.createPost(req.body, res.locals.payload.id);
+    return res.status(201).json(post);
+};
 
-const getPosts = async (req, res) => {
-    const user = await postService.getPosts(req.body);
+const getPosts = async (_req, res) => {
+    const user = await postService.getPosts();
     return res.status(200).json(user);
+};
+
+const getPostById = async (req, res) => {
+    const post = await postService.getPostById(req.params.id);
+    if (!post) return res.status(404).json({ message: 'Post does not exist' });
+    return res.status(200).json(post);
 };
 
 module.exports = {
     getPosts,
+    newPost,
+    getPostById,
 };
