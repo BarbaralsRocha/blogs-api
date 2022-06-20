1aa4ba2ccd4028578496c139c2d7f9a49450f83a
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-06-20 15:59:44 -0300

    fix: change name variavel

--- src/middlewares/categoryValidation.js
+++ src/middlewares/categoryValidation.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-const productsDTO = Joi.object({
+const categoryDTO = Joi.object({
     name: Joi.string().required().messages({
     'any.required': '400|{{#label}} is required',
     'string.base': '400| must be a string',
@@ -8,7 +8,7 @@ const productsDTO = Joi.object({
 });
 
 const categoryValidation = (req, res, next) => {
-    const { error } = productsDTO.validate(req.body);
+    const { error } = categoryDTO.validate(req.body);
     if (error) {
         const [code, message] = error.message.split('|');
         return res.status(code).json({ message });
